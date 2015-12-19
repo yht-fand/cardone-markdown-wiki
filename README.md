81f6a12840885789ece4633d9ebc1c54feaea5b3
yao hai tao <addday@gmail.com>
2015-12-19 22:57:20 +0800

    基本功能完成

---
+++ README.md
@@ -0,0 +1,3 @@
+# cardone markdown wiki
+
+基于静态 markdown 文件的 wiki
---
+++ app/IndexRouter.js
@@ -0,0 +1,25 @@
+define(['marked', 'backbone'], function (marked) {
+    'use strict';
+    return Backbone.Router.extend({
+        routes: {
+            '': 'readmeMd',
+            "*path.md": "pathMd",
+            "*path": "path"
+        },
+        readmeMd: function () {
+            this.pathMd('README')
+        },
+        pathMd: function (path) {
+            var mdFilePath = path + '.md';
+
+            require(['text!' + mdFilePath], function (mdFile) {
+                $('body').html(marked(mdFile));
+            }, function (err) {
+                $('body').html('');
+            });
+        },
+        path: function (path) {
+            this.pathMd(path + '/README')
+        }
+    });
+});
