e17a50ca0ea47b29193ee20baf0120c0a5a1c08d
yao hai tao <addday@gmail.com>
2016-06-15 16:02:38 +0800

    改时说明

--- app/IndexRouter.js
+++ app/IndexRouter.js
@@ -14,6 +14,8 @@ define(['marked', 'backbone'], function (marked) {
 
             require(['text!' + mdFilePath], function (mdFile) {
                 $('#region-main').html(marked(mdFile));
+
+                $('#region-main table').addClass('table table-bordered table-hover table-condensed');
             }, function (err) {
                 $('#region-main').html('');
             });
